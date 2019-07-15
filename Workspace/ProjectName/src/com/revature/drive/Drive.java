3b8ba53c7ecc11b60367456f38915587085d38f6
Leoschenck <leoschenck@gmx.de>
2019-07-15 16:48:51 -0400

    added ProjectName

---
+++ Workspace/ProjectName/src/com/revature/drive/Drive.java
@@ -0,0 +1,27 @@
+package com.revature.drive;
+public class Drive {
+// namespace to organize related classesäüö
+/*
+ * comment here~~
+ * 
+  OR HERE
+ */
+	/*
+	 * naming conventions
+	 * Classes & projects are capitalized (Pascal casing)
+	 * 
+	 * methods and variables - camelCase
+	 * packagenames all lowercase, sperated by periods
+	 * Constants - ALL_CAPS_DAD
+	 */
+	//Mainmethod is the ENTRYPOINT
+	public static void main(String[] ogre) {
+		/*
+		 * public - everyone can see it (access motifier)
+		 * static - cannot be changed, can be accessed without 
+		 * instantiation; belongs to the class
+		 * 	String[] args - parameter utilized in this method
+		 */
+		System.out.println("KTHXBYE");	
+	}
+}
