10959abdf4f9d92506fb67a1350183b3ad5783c0
Yogeros Smith <yogeross@buffalo.edu>
2019-07-15 16:50:52 -0400

    added week 1 and projectmayhem

---
+++ Class/ProjectMayhem/src/com/revature/driver/Driver.java
@@ -0,0 +1,34 @@
+package com.revature.driver;
+// package- namespace to organise related classes and interfaces
+public class Driver {
+	//single line comment
+	
+	/*multi
+	 * line 
+	 * comment
+	 */
+	
+	
+	/*
+	 * Naming Conventions
+	 * classes and projects- Capitalize each word(Pascal casing)
+	 * methods and variables- camelCase
+	 * packages- all lower-case, separated by periods
+	 * constants- ALL CAPS
+	 */
+	
+	
+	/*
+	 * Main method is the entry point
+	 */
+	public static void main(String[] args) {
+		/*
+		 *Access Modifiers
+		 *public- anything can see it
+		 *static- belongs to class, does not need to be instantiated
+		 *String[] args- parameter utilized in this method
+		 */
+		System.out.print("Goodbye Earth");
+	}
+	
+}
