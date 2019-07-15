6920c69597897c8ae2cec50028e8d19c7003deef
Jason Knighten <jason.knighten@revature.com>
2019-07-15 16:48:32 -0400

    added week 1 and projectmayhem

---
+++ Week1/ProjectMayhem/src/com/revature/driver/Driver.java
@@ -0,0 +1,32 @@
+package com.revature.driver;
+//package- namespace to organize related class/interfaces,etc
+public class Driver {
+	//single line comment
+	/*
+	 * This
+	 * is
+	 * a 
+	 * really
+	 * long
+	 * multi
+	 * line
+	 * comment
+	 */
+	/*
+	 * Naming Conventions
+	 * classes and projects-Pascal- Capitialize each word
+	 * methods and variables- camelCase ex firstSecondThird
+	 *Package names: all lowercase, separated by periods
+	 *constants: ALL_CAPS_LOLZ
+	 * 
+	 */
+	//Main Method is the entry point
+	public static void main(String[] args) {
+		/*
+		 * public- access modifiers, anything can see it
+		 * static- belongs to class, does not need to be instantiated
+		 * String []args- parameter utilized in this method
+		 */
+		System.out.println("Goodbye,earth");
+	}
+}
