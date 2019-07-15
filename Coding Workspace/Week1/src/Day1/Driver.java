35c719f5af35bf89d529bf64812e0fcd290f8511
Bradley Campbell <bradley@USF-Guest-Wifi-46-45.laptops.usf.edu>
2019-07-15 16:49:40 -0400

    Week 1 Code

---
+++ Coding Workspace/Week1/src/Day1/Driver.java
@@ -0,0 +1,26 @@
+package Day1; //package is an organized name space for related classes, interfaces, etc.
+
+
+
+public class Driver {
+	
+	/*
+	 * Naming Conventions
+	 * package names: all lowercase, separated by periods
+	 * classes & projects: Capitalize each word
+	 * methods and variables: camelCase
+	 * constants: ALL_UPPERCASE
+	 */
+	
+	public static void main(String[] args) { //Main method for program to run
+		
+		/*
+		 * public: access modifier that allows everything to see it
+		 * static: belongs to class, does not need to be instantiated
+		 * String[] args: parameters utilized by this method
+		 */
+		
+		System.out.println("Toodaloo Planet");
+	}
+	
+}
