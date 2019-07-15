97ca85a4a30ba99430d314811da05f87c37e5408
ljapplebaum <ljapplebaum@gmail.com>
2019-07-15 16:48:49 -0400

    Week 1 Goodbye Earth Committed

---
+++ Week1/ProjectMayhem/src/com/revature/driver/Driver.java
@@ -0,0 +1,30 @@
+package com.revature.driver;
+	//Package- namespace in which to organize affiliated classes
+public class Driver {
+	//comment
+	/*
+	 * c
+	 * o
+	 * m
+	 * m
+	 * e
+	 * n
+	 * t
+	 */
+	//classes and projects-		Example
+	//methods and variables-	exampleTwo
+	//package names-			example.three
+	//constants-				EXAMPLE_FOUR
+	//main method- entry point
+	
+	public static void main(String []args) {
+		/*
+		 * public- can be accessed anywhere
+		 * static- belongs to class, need not be instantiated
+		 * String []args- parameters for this method
+		 * protected- can be accessed by child classes
+		 * private- can only be accessed by class
+		 */
+		System.out.print("Goodbye, Earth!\n");
+	}
+}
