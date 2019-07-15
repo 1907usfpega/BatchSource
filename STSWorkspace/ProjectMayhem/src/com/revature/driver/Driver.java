0383e5cc5d66e5667d0eb508621862beddbef64b
jaxondavis <jackdavis7118@gmail.com>
2019-07-15 16:48:30 -0400

    added week 1 and projectmayhem

---
+++ STSWorkspace/ProjectMayhem/src/com/revature/driver/Driver.java
@@ -0,0 +1,41 @@
+package com.revature.driver;
+//packages are namespaces to organize related classes, interfaces, etc
+public class Driver 
+{
+	//single line comment
+	/*block comment
+	 * 
+	 * This
+	 * is
+	 * a
+	 * really
+	 * long
+	 * multi
+	 * line
+	 * comment
+	 * 
+	 */
+	
+	/*
+	 * naming conventions
+	 * projects and classes: CapitolCase also Pascal case
+	 * methods and variables: camelCase
+	 * Package names: all lowercase, separated by periods
+	 * constants: ALL_CAPS_NO_LOWERS
+	 * 
+	 */
+	
+	public static void main(String[] args) 
+	{
+		//this is the entry point of the class
+		
+		/*
+		 * Access modifiers
+		 * Public: anything can access
+		 * static: belongs to the class (not the object), and does not need to be instantiated
+		 * String[] args: a parameter utilized in this methods  
+		 */
+		System.out.println("Goodbye, cruel world!");
+	}
+
+}
