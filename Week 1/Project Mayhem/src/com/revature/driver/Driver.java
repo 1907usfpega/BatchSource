84d763eccf2cad8df7543f47426b34c3a2259b41
itizeddieh <eddie.m.heredia@gmail.com>
2019-07-15 16:58:43 -0400

    day one notes

---
+++ Week 1/Project Mayhem/src/com/revature/driver/Driver.java
@@ -0,0 +1,33 @@
+package com.revature.driver;
+//package: a namespace to organized a related classes/interfaces, etc
+public class Driver {
+	// Single line comment
+	/*
+	 * Block 
+	 * Comment
+	 */
+	/*
+	 * Naming Conventions
+	 * Classes and Projects:Pascal: Capitalize each word
+	 * methods and variables: camelCase ex: firstSecoundThird
+	 * package: all lowercase, separated by periods
+	 * constants: ALL_CAPS
+	 */
+	//Main methods is the entry point
+	public static void main(String[] args) {
+		/*
+		 * public: access modifier, anything can see it
+		 * static: belongs to the class, does not need to be instantiated
+		 * String[] args: parameter utilized in this methods
+		 */
+		System.out.println("Dab on the haters!");
+		/*
+		 * Members of a class: 	can have different forms
+		 * Instance variables: 	property that belongs to the specific object
+		 * Static variables: 	belongs to the class/all instances
+		 * Instance methods: 	behavior related to a specific object
+		 * Static methods: 		relative to the entire class
+		 * Constructors: 		instantiates the class using the keyword "new"
+		 */
+	}
+}
