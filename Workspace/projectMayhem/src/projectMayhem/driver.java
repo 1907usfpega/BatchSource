8f148cabe6c891f357297e3c94d8868f9309590d
darrellgrant <darrellgrantinfo@gmail.com>
2019-07-15 16:51:55 -0400

    made changes

---
+++ Workspace/projectMayhem/src/projectMayhem/driver.java
@@ -0,0 +1,25 @@
+package projectMayhem;
+
+public class driver {
+	//single line comment
+	/*this is
+	 *  a 
+	 * multi-line comment
+	 * package is a namespace to 
+	 * organize classes and interfaces
+	 * package names are lowercase separated by periods
+	 * constants are all caps (usually delimited by underscores)
+	 * ar
+	 *  */
+	public static void main(String[] args) {
+		/* public - access modifier, anyone can see
+		 * static- belongs t class, does not need to be instantiated
+		 * String [] args -parameter utilized in this method
+		 * 
+		 * */
+		System.out.println("Goodbye Earth");
+	}
+	
+	
+	
+}
