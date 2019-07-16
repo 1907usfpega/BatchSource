503393c93816fe1b096033c07c65cdfaf9e8a5fa
zthompson2350 <ZAT0002@auburn.edu>
2019-07-16 09:22:42 -0400

    initial commit

---
+++ Week 1/ProjectMayham/ProjectMayhem/src/com/revature/driver/Driver.java
@@ -0,0 +1,23 @@
+package com.revature.driver;
+
+public class Driver {
+
+	
+	/*
+	 * Naming Conventions
+	 * Classes and projects - pascal - Capitalize each word
+	 * metyhods and variables - camelCase
+	 * Package names - all lowercase, seperated by periods
+	 * constants - ALL_CAPS_LOLX
+	 */
+	public static void main(String[] args) {
+		/*
+		 * public - access modifiers, anything can see it
+		 * static - belongs to class, does not need to be instantiated
+		 * String [] args - paramter utilized in this method
+		 */
+
+		System.out.println("Goodbye Earth!");
+	}
+
+}
