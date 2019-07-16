5d784c0d1211d8aad0484f9996df0de00528bd46
Deonta Kilpatrick <deonta.kilpatrick@gmail.com>
2019-07-16 10:01:45 -0400

    Added Project Mayhem from Day 1  (Monday)

---
+++ Week1/ProjectMayhem/src/com/revature/Driver.java
@@ -0,0 +1,39 @@
+package com.revature;
+
+public class Driver {
+
+	//single line comment
+	/*
+	 * long
+	 * multiline
+	 * comment
+	 */
+
+	/*
+	 * Naming Connventions
+	 * classes and projects- PascalCase- Capitalize each word
+	 * Package names: all lowercase, separated by periods
+	 * constants: ALL_CAPS_LOLZ
+	 */
+
+	//Main Method is the entry point
+	
+	public static void main(String[] args) {
+		/*
+		 * public - access modifiers, anything can see it
+		 * static - belongs to class, does not need to be instantiated
+		 * String[] args - parameter utilized in this method
+		 */
+		System.out.println("Goodbye, earth");
+		
+		/*
+		 * Members of a class - can have different forms
+		 * Instance variables - property that belongs to a specific object
+		 * Static variables - belongs to class/ all instances 
+		 * Instance methods-behavior related to a specific object
+		 * Static methods- relative to the entire class
+		 * Constructors-instantiates the class using the keyword "new"
+		 */
+	}
+
+}
