5e76639d002ecff065c7de6bf4de98854422edde
ljapplebaum <ljapplebaum@gmail.com>
2019-07-18 12:24:50 -0400

    Uploaded to migrate to loaner pc

--- Week1/ProjectMayhem/src/com/revature/corejavaassignment/Q19.java
+++ Week1/ProjectMayhem/src/com/revature/corejavaassignment/Q19.java
@@ -11,12 +11,16 @@ public class Q19 {
 		System.out.println("Numbers 1 - 10:\n" + firstTen);
 
 		int evenSum = 0;
+		int oddSum = 0;
 		for(int i = 0; i < 10; i++) {
-			if(firstTen[0] % 2 == 0) {
-				
+			if(firstTen.get(i) % 2 == 0)
+				evenSum += firstTen.get(i);
+			else
+				oddSum += firstTen.get(i);
 		}
-			
-		}
+		System.out.println("The sum of the even numbers = " + evenSum);
+		System.out.println("The sum of the odd numbers = " + oddSum);
+		
 		for(int i = 9; i >= 0; i--) {
 			if(primeNums.get(i) > 100)
 				primeNums.remove(i);
