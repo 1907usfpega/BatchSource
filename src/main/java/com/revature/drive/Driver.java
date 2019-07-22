2d9674f1f89e2650cd77e67e2a5b07a11765945d
Dayana Viana <dayanatsv@gmail.com>
2019-07-22 13:43:59 -0400

    FINAL. READY.

--- src/main/java/com/revature/drive/Driver.java
+++ src/main/java/com/revature/drive/Driver.java
@@ -25,97 +25,97 @@ import com.revature.utils.MyFunctions;
 public class Driver {
 
 	public static void main(String[] args) {
-//		System.out.println("Question 1");
-//		Q1.bubbleSort();
-//		printLine();
-//
-//		System.out.println("Question 2");
-//		Q2.displayFibonacci();
-//		printLine();
-//
-//		System.out.println("Question 3");
-//		Q3.reverseString("Framework");
-//		printLine();
-//
-//		System.out.println("Question 4");
-//		Q4.calcFactorial(9);
-//		printLine();
-//
-//		System.out.println("Question 5");
-//		Q5.substring("dayana", 5);
-//		printLine();
-//
-//		System.out.println("Question 6");
-//		Q6.isEven(18);
-//		printLine();
-//
-//		System.out.println("Question 7");
-//		Q7.sortEmployees();
-//		printLine();
-//
-//		System.out.println("Question 8");
-//		Q8.savePalindromes();		
-//		printLine();
-//
-//		System.out.println("Question 9");
-//		Q9.printPrime();
-//		printLine();
-//
-//		System.out.println("Question 10");
-//		Q10.findMin(93, 67);
-//		printLine();
-//
-//		System.out.println("Question 11");
-//		Q11.accessVariable();
-//		printLine();
-//
-//		System.out.println("Question 12");
-//		Q12.printEven();
-//		printLine();
-//
-//		System.out.println("Question 13");
-//		Q13.printTriagle();
-//		printLine();
-//
-//		System.out.println("Question 14");
-//		Q14.doSwitch(1);
-//		Q14.doSwitch(2);
-//		Q14.doSwitch(3);
-//		printLine();
-//
-//		System.out.println("Question 15");
-//		Q15 q15 = new Q15();
-//		q15.printAddition(24, 24);
-//		q15.printSubtraction(25, 13);
-//		q15.printMultiplication(3, 14);
-//		q15.printDivision(12, 3);
-//		printLine();
-//
-//		System.out.println("Question 16");
-//		Q16.countInput(args);
-//		printLine();
-//
-//		System.out.println("Question 17");
-//		Q17.calcInterest();
-//		printLine();
-//
+		System.out.println("Question 1");
+		Q1.bubbleSort();
+		printLine();
+
+		System.out.println("Question 2");
+		Q2.displayFibonacci();
+		printLine();
+
+		System.out.println("Question 3");
+		Q3.reverseString("Framework");
+		printLine();
+
+		System.out.println("Question 4");
+		Q4.calcFactorial(9);
+		printLine();
+
+		System.out.println("Question 5");
+		Q5.substring("dayana", 5);
+		printLine();
+
+		System.out.println("Question 6");
+		Q6.isEven(18);
+		printLine();
+
+		System.out.println("Question 7");
+		Q7.sortEmployees();
+		printLine();
+
+		System.out.println("Question 8");
+		Q8.savePalindromes();		
+		printLine();
+
+		System.out.println("Question 9");
+		Q9.printPrime();
+		printLine();
+
+		System.out.println("Question 10");
+		Q10.findMin(93, 67);
+		printLine();
+
+		System.out.println("Question 11");
+		Q11.accessVariable();
+		printLine();
+
+		System.out.println("Question 12");
+		Q12.printEven();
+		printLine();
+
+		System.out.println("Question 13");
+		Q13.printTriagle();
+		printLine();
+
+		System.out.println("Question 14");
+		Q14.doSwitch(1);
+		Q14.doSwitch(2);
+		Q14.doSwitch(3);
+		printLine();
+
+		System.out.println("Question 15");
+		Q15 q15 = new Q15();
+		q15.printAddition(24, 24);
+		q15.printSubtraction(25, 13);
+		q15.printMultiplication(3, 14);
+		q15.printDivision(12, 3);
+		printLine();
+
+		System.out.println("Question 16");
+		Q16.countInput(args);
+		printLine();
+
+		System.out.println("Question 17");
+		Q17.calcInterest();
+		printLine();
+
 		System.out.println("Question 18");
 		Q18 q18 = new Q18();
 		q18.checkUppercase_print("daYana");
 		q18.convertToUppercase_print("Hello there");
 		q18.addTen_print();
 		printLine();
-//
-//		System.out.println("Question 19");
-//		Q19 q19 = new Q19();
-//		Q19.sumOdd(q19.listArray);
-//		Q19.sumEven(q19.listArray);
-//		printLine();
-//
-//		System.out.println("Question 20");
-//		Q20 q20 = new Q20();
-//		
-//		System.out.println("\nThe End");
+
+		System.out.println("Question 19");
+		Q19 q19 = new Q19();
+		Q19.sumOdd(q19.listArray);
+		Q19.sumEven(q19.listArray);
+		printLine();
+
+		System.out.println("Question 20");
+		Q20 q20 = new Q20();
+		
+		System.out.println("\nThe End");
 
 	}
 	
