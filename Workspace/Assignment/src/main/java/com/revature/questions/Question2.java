c771a9defd4b77bdc3aaef32875e91b1f9544a71
Leoschenck <leoschenck@gmx.de>
2019-07-29 15:38:36 -0400

    FOLLOW THIS PATH TO ZACHS QUESTIONSgit add Question2.java    jkjk, just mine. Also resolved a mergeconflict that went unnoticed earlier. If that's not okay, just don't count Question 2.

--- Workspace/Assignment/src/main/java/com/revature/questions/Question2.java
+++ Workspace/Assignment/src/main/java/com/revature/questions/Question2.java
@@ -4,13 +4,9 @@ import java.util.Arrays;
 
 public class Question2 {
 	static int[] fibos = new int[25];
-<<<<<<< HEAD
-	public static int[] doTask(){
-		for (int i = 0; i < fibos.length; i++) {//for 23 times, it will add the last two fibos to create the current one. First two are 0 & 1
-=======
 	public static int doTask(){
-		for (int i = 0; i < fibos.length; i++) {
->>>>>>> TerryLeo
+		for (int i = 0; i < fibos.length; i++) {//for 23 times, it will add the last two fibos to create the current one. First two are 0 & 1
+
 			if(i==0 || i==1) {
 				fibos[i] = i;
 				continue;
