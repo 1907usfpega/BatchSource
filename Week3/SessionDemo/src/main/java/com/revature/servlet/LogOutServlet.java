3da7566296648ba8362ebf80219303aa4477f4a6
Justin <jhua8837@gmail.com>
2019-08-02 16:17:10 -0400

    updated week 3 servlet session

---
+++ Week3/SessionDemo/src/main/java/com/revature/servlet/LogOutServlet.java
@@ -0,0 +1,40 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogOutServlet
+ */
+public class LogOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogOutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		request.getRequestDispatcher("link.html").include(request, response);
+		HttpSession session=request.getSession();
+		session.invalidate();
+		
+		out.print("You are successfully logged out");
+		out.close();
+	}
+
+}
