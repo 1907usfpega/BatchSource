e07b73709112ed2cf7e39796febdf8221c8427d7
Justin <jhua8837@gmail.com>
2019-08-03 10:57:57 -0400

    added week 3 examples

--- Week3/ForwardRedirectDemo/src/main/java/com/revature/servlet/HomeServlet.java
+++
@@ -1,7 +0,0 @@
-package com.revature.servlet;
-
-public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	protected void doGet
-}
--- Week3/ForwardRedirectDemo/src/main/java/com/revature/servlet/LoginServlet.java
+++
@@ -1,9 +0,0 @@
-package com.revature.servlet;
-
-public class LoginServlet extends HttpServlet {
-	private static final long 
-	
-	protected void doGet(HttpServletRequest request. HttpServletResponse resp
-			System.out.println("in doGet of LoginServlet");
-			RequestDispatcher rd+ request.getRespo
-}
