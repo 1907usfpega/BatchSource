8f2b492f0fb3fa982d88128ae9cc3110a4abef39
Terry Kow Zhi Im <terryim@USF-Gold-Wifi-226-59-98.laptops.usf.edu>
2019-08-05 15:38:40 -0400

    js connected with html

---
+++ TRMS/JRMS/src/main/webapp/WEB-INF/index.js
@@ -0,0 +1,12 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function logIn() {
+    document.getElementById("loader");
+    console.log("login clicked");
+}
